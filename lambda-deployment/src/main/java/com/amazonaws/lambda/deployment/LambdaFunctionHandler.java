9ea367fcd08d2da3f1e046ed2989bd31884c6f3e
prafful namdev <prafful.namdev18@gmail.com>
2021-12-06 12:18:58 +0530

    messages

--- lambda-deployment/src/main/java/com/amazonaws/lambda/deployment/LambdaFunctionHandler.java
+++ lambda-deployment/src/main/java/com/amazonaws/lambda/deployment/LambdaFunctionHandler.java
@@ -1,5 +1,7 @@
 package com.amazonaws.lambda.deployment;
 
+import java.util.Map;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
@@ -7,10 +9,10 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
  * https://www.youtube.com/watch?v=aGI4Wlm5c9U
  *
  */
-public class LambdaFunctionHandler implements RequestHandler<Object, String> {
+public class LambdaFunctionHandler implements RequestHandler<Map<String, Object>, String> {
 
     @Override
-    public String handleRequest(Object input, Context context) {
+    public String handleRequest(Map<String, Object> input, Context context) {
         context.getLogger().log("Input: " + input);
 
         // TODO: implement your handler
